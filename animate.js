6bdc2c00545d0279b1c9c0cdc56c8cbe27df0e73
Sabarish-2 <sabarishh03@gmail.com>
2023-10-31 20:30:46 +0530

    First!

---
+++ animate.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const blinkJourney = document.getElementById("blink");
+    try {
+        if (blinkJourney){
+        setInterval(function() {
+            blinkJourney.style.color = (blinkJourney.style.color === "blue") ? "orange" : "blue";
+        }, 750);}
+    } catch (error) {}
+
+    try {
+        var typed = new Typed('#element', {
+            strings: ['App Developer', 'Website Developer', 'Game Developer'],
+            typeSpeed: 50,
+            loop:true
+        });
+    } catch (error) { }
+
+
+});
